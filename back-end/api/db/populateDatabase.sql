8bccb45815179ce73e8a1ee38b18d678ac1a0ecd
Lucas Pereira Cotrim <lucaspcotrim@gmail.com>
2023-04-19 21:18:57 -0300

    feat: Added populateDatabase.sql file

---
+++ back-end/api/db/populateDatabase.sql
@@ -0,0 +1,4 @@
+INSERT INTO applications("name","whatsapp_string","front_end_exp","front_end_exp_years","back_end_exp","back_end_exp_years","db_exp","db_exp_years",
+						             "camunda_exp","healthcare_exp","comments")
+VALUES ('Test name 1','(11) 91234-5678','Muito experiente',3,'Experiente',2,'Alguma experiência',1,TRUE,FALSE,'Test comment 1'),
+		   ('Test name 2','(20) 91234-5678','Experiente',1,'Muito Experiente',3,'Alguma experiência',1,FALSE,FALSE,'Test comment 2');
